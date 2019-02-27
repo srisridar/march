6a174434c3e52e4857286c225496418f7465298e
srisridar <sreedhar.fcc@gmail.com>
2019-02-27 08:43:59 +0530

    indexing assignment

---
+++ agecalculater.py
@@ -0,0 +1,17 @@
+# Age calculater
+
+# data 
+
+currentYear = 2019
+yob=int(input("please enter your year of birth :"))
+
+
+# processing
+
+#yob=int(yob)
+age = currentYear - yob
+
+# Display
+
+print("Your age is:",age)
+
